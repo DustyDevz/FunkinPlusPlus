187561110e9ebff376da58a8df05a5a5799d8b7f
DustyDevz <matthewvigoncalves@gmail.com>
2026-07-31 09:22:09 -0400

    VERY IMPORTANT

--- funkin/src/main.cpp
+++ funkin/src/main.cpp
@@ -11,30 +11,7 @@
   Project: https://github.com/DustyDevz/FunkinPlusPlus
 */
 
-#include "app/window.hpp"
-#include "render/render.hpp"
-
 int main() {
     LOG_PRINT("hi");
-
-    Funkin::App::Window window;
-    if (!window.Init("window test", 1280, 720)) {
-        LOG_CRIT("Failed to initialize window");
-        return 1;
-    }
-
-    Funkin::Render::Render renderer;
-    if (!renderer.Init(window)) {
-        LOG_CRIT("Failed to initialized renderer");
-        return 1;
-    }
-
-    while (window.PollEvents()) {
-        renderer.BeginFrame();
-        renderer.EndFrame();
-    }
-
-    renderer.Shutdown();
-    window.Shutdown();
     return 0;
 }
